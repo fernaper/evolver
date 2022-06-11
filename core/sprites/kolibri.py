60a9e56781a177a64214609c26c2f639a4f98018
Fernando <fernaperg@gmail.com>
2022-06-11 18:20:20 +0200

    :sparkles: Added camera control :wrench: Now multiple kolibries are spawned at the beginning :art: Added initial basic GUI :wrench: Kolibris now have different speeds, angle rotations, sizes :wrench: Kolibris are now spaned at different positions and angles :wrench: Childs now grew up and are spawned on the parent position

--- core/sprites/kolibri.py
+++ core/sprites/kolibri.py
@@ -4,6 +4,7 @@ import random
 
 from dataclasses import dataclass, field
 from enum import Enum, auto
+from typing import Optional
 
 from core import config
 from .base import (
@@ -26,8 +27,11 @@ class KolibriActions(Enum):
 
 @dataclass
 class KolibriProperties():
-    spawn_time: int = field(default_factory=lambda : random.randint(250, 700))
-    max_live: int = field(default_factory=lambda : random.randint(150, 800))
+    spawn_time: int = field(default_factory=lambda : random.randint(250, 1000))
+    max_live: int = field(default_factory=lambda : random.randint(150, 3000))
+    max_speed: float = field(default_factory=lambda : random.uniform(0.2, 15))
+    max_angle_speed: float = field(default_factory=lambda : random.uniform(0.1, 3.5))
+    max_size: float = field(default_factory=lambda : random.uniform(0.75, 3))
 
 
 @dataclass
@@ -43,25 +47,24 @@ class KolibriSprite(arcade.Sprite):
     FRAMES_TO_UPDATE = 12
     FRAMES_TO_ACTION = 24
 
-    def __init__(self, kolibri_list: arcade.SpriteList):
+    def __init__(self, kolibri_list: arcade.SpriteList, center_x: Optional[int] = None, center_y: Optional[int] = None):
         super().__init__()
         self.id = guid()
-        self.scale: float = 1.5
         self.speed: float = 0
-        self.max_speed: float = 8
-        self.max_angle_speed: float = 2
-        self.center_x: int = 32 * self.scale
-        self.center_y: int = 32 * self.scale
         self.diet = random.choice(list(KolibriDiet))
         self.action_to_textures = self.build_action_to_texture()
         self.properties = KolibriProperties()
         self.counters = KolibriCounters()
         self.__active_action = KolibriActions.idle
+        self.angle = random.uniform(0, 360)
+        self.center_x: int = 32 * self.properties.max_size if center_x is None else center_x
+        self.center_y: int = 32 * self.properties.max_size if center_y is None else center_y
+        self.scale: float = 1.5
 
         # Shared reference to kolibri list
         self.kolibri_list = kolibri_list
         self.kolibri_list.append(self)
-        print(f'Spawned #{self.id}')
+        #print(f'Spawned #{self.id}')
 
     def build_action_to_texture(self):
         action_to_textures = {
@@ -107,15 +110,16 @@ class KolibriSprite(arcade.Sprite):
         self.counters.texture_counter = 0
 
     def update(self):
+        self.update_scale()
         self.update_action()
 
         if self.action == KolibriActions.walking:
-            self.speed = self.max_speed
+            self.speed = self.properties.max_speed
         else:
             self.speed = 0
 
         if self.action not in (KolibriActions.sleeping, KolibriActions.eating, KolibriActions.dying):
-            self.change_angle = self.max_angle_speed * random.choice([1,-1])
+            self.change_angle = self.properties.max_angle_speed * random.choice([1,-1])
             if self.counters.spawn_counter % self.properties.spawn_time == 0:
                 self.spawn_child()
 
@@ -129,14 +133,23 @@ class KolibriSprite(arcade.Sprite):
         self.update_animation()
         self.update_counters()
 
+    def update_scale(self):
+        self.scale = max(
+            self.properties.max_size / 2,
+            min(
+                self.properties.max_size,
+                self.properties.max_size * (self.counters.live_counter / (self.properties.spawn_time / 3))
+            )
+        )
+
     def update_action(self):
         if self.counters.live_counter >= self.properties.max_live:
             if self.action != KolibriActions.dying:
                 self.action = KolibriActions.dying
             return
+        self.counters.action_counter = self.counters.action_counter % KolibriSprite.FRAMES_TO_ACTION
         if self.counters.action_counter == 0:
             self.action = random.choice([KolibriActions.walking, KolibriActions.idle])
-        self.counters.action_counter = self.counters.action_counter % KolibriSprite.FRAMES_TO_ACTION
 
     def update_animation(self):
         textures = self.action_to_textures[self.__active_action]
@@ -149,9 +162,10 @@ class KolibriSprite(arcade.Sprite):
                 self.remove_from_sprite_lists()
                 return
         self.texture = textures[frame]
+        #print(self.action, '---->', self.counters.action_counter)
 
     def spawn_child(self) -> 'KolibriSprite':
-        return KolibriSprite(self.kolibri_list)
+        return KolibriSprite(self.kolibri_list, self.center_x, self.center_y)
 
     def reset_counters(self):
         self.counters.texture_counter += 0
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import arcade
+import random
+
+from pyglet.math import Vec2
 
 from core import config
 from core.sprites.kolibri import KolibriSprite
@@ -6,6 +9,8 @@ from core.sprites.kolibri import KolibriSprite
 
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
+CAMERA_SPEED = 0.99
+STARTING_KOLIBRIS = 100
 SCREEN_TITLE = 'Evolver'
 
 
@@ -15,19 +20,47 @@ class Evolver(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.AMAZON)
         self.kolibri_list = None
+        self.current_position = [0, 0]
+        self.move_camera = [0,0]
+        self.camera_sprites = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.camera_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         self.kolibri_list = arcade.SpriteList()
-        KolibriSprite(self.kolibri_list)
+
+        for _ in range(STARTING_KOLIBRIS):
+            KolibriSprite(
+                self.kolibri_list,
+                center_x=random.randint(0, SCREEN_WIDTH),
+                center_y=random.randint(0, SCREEN_HEIGHT)
+            )
 
     def on_draw(self):
         """
         Render the screen.
         """
         self.clear()
+        self.draw_sprites()
+        self.draw_gui()
+
+    def draw_sprites(self):
+        self.camera_sprites.use()
         for kolibri_sprite in self.kolibri_list:
             kolibri_sprite.draw(pixelated=True)
+
+    def draw_gui(self):
+        self.camera_gui.use()
+        arcade.draw_rectangle_filled(
+            self.width // 2, 20, self.width, 40, arcade.color.ALMOND
+        )
+        text = (
+            f'Scroll value: ({self.camera_sprites.position[0]:5.1f}, '
+            f'{self.camera_sprites.position[1]:5.1f}); Alive: {len(self.kolibri_list)}'
+        )
+        arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
+
 
     def on_update(self, delta_time):
         """
@@ -36,6 +69,13 @@ class Evolver(arcade.Window):
         need it.
         """
         self.kolibri_list.update()
+        # Scroll the screen to the player
+        self.scroll_to_position()
+
+    def scroll_to_position(self):
+        self.current_position[0] += self.move_camera[0]
+        self.current_position[1] += self.move_camera[1]
+        self.camera_sprites.move_to(Vec2(*self.current_position), CAMERA_SPEED)
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -45,17 +85,13 @@ class Evolver(arcade.Window):
         https://api.arcade.academy/en/latest/arcade.key.html
         """
         if key == arcade.key.UP or key == arcade.key.W:
-            for k in self.kolibri_list:
-                k.speed = k.max_speed
+            self.move_camera[1] = 10
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            for k in self.kolibri_list:
-                k.speed = - k.max_speed
+            self.move_camera[1] = -10
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            for k in self.kolibri_list:
-                k.change_angle = k.max_angle_speed
+            self.move_camera[0] = -10
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            for k in self.kolibri_list:
-                k.change_angle = - k.max_angle_speed
+            self.move_camera[0] = 10
         elif key == arcade.key.SPACE:
             self.kolibri_list.append(KolibriSprite())
 
@@ -64,17 +100,13 @@ class Evolver(arcade.Window):
         Called whenever the user lets off a previously pressed key.
         """
         if key == arcade.key.UP or key == arcade.key.W:
-            for k in self.kolibri_list:
-                k.speed = 0
+            self.move_camera[1] = 0
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            for k in self.kolibri_list:
-                k.speed = 0
+            self.move_camera[1] = 0
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            for k in self.kolibri_list:
-                k.change_angle = 0
+            self.move_camera[0] = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            for k in self.kolibri_list:
-                k.change_angle = 0
+            self.move_camera[0] = 0
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
